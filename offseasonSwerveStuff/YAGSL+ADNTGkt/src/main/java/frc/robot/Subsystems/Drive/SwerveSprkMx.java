7e75777bf91b046a207dc7951fca5c730e01e2ed
masonmm3 <53064865+masonmm3@users.noreply.github.com>
2023-07-03 20:08:26 -0400

    imported YAGSL + hardware interface code

--- offseasonSwerveStuff/YAGSL+ADNTGkt/src/main/java/frc/robot/Subsystems/Drive/SwerveSprkMx.java
+++ offseasonSwerveStuff/YAGSL+ADNTGkt/src/main/java/frc/robot/Subsystems/Drive/SwerveSprkMx.java
@@ -4,6 +4,66 @@
 
 package frc.robot.Subsystems.Drive;
 
+import java.io.File;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import swervelib.SwerveDrive;
+import swervelib.parser.SwerveControllerConfiguration;
+import swervelib.parser.SwerveDriveConfiguration;
+import swervelib.parser.SwerveParser;
+import swervelib.telemetry.SwerveDriveTelemetry;
+import swervelib.telemetry.SwerveDriveTelemetry.TelemetryVerbosity;
+
 /** Add your docs here. */
-public class SwerveSprkMx {
+public class SwerveSprkMx implements SwerveIO {
+    private final SwerveDrive swerveDrive;
+
+    
+    public SwerveSprkMx(File directory) {
+        SwerveDriveTelemetry.verbosity = TelemetryVerbosity.HIGH;
+        try {
+            swerveDrive = new SwerveParser(directory).createSwerveDrive();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public SwerveSprkMx(SwerveDriveConfiguration driveCfg, SwerveControllerConfiguration controllerCfg){
+        swerveDrive = new SwerveDrive(driveCfg, controllerCfg);
+    }
+
+    public void drive(Translation2d translation, double rotation, boolean fieldRelative, boolean isOpenLoop) {
+        swerveDrive.drive(translation, rotation, fieldRelative, isOpenLoop);
+    }
+
+    //getters
+
+    public Rotation2d getHeading() {
+        return swerveDrive.getYaw();
+    }
+
+    public ChassisSpeeds getTargetSpeeds(double xInput, double yInput, double headingX, double headingY) {
+        double xinput = Math.pow(xInput, 3);
+        double yinput = Math.pow(yInput, 3);
+        return swerveDrive.swerveController.getTargetSpeeds(xinput, yinput, headingX, headingY, getHeading().getRadians());
+    }
+
+    public ChassisSpeeds getFieldVelocity() {
+        return swerveDrive.getFieldVelocity();
+    }
+
+    public Pose2d getPose() {
+        return swerveDrive.getPose();
+    }
+
+    public SwerveDriveConfiguration getSwerveDriveConfig() {
+        return swerveDrive.swerveDriveConfiguration;
+    }
+
+    public void periodicTask() {
+        swerveDrive.updateOdometry();
+    }
 }
