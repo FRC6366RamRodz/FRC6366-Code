1131de0982b484e101049210af27f40cc29cc50f
masonmm3 <53064865+masonmm3@users.noreply.github.com>
2023-07-24 16:43:01 -0400

    pathplanner  updates

--- offseasonSwerveStuff/YAGSL+ADNTGkt/src/main/java/frc/robot/RobotContainer.java
+++ offseasonSwerveStuff/YAGSL+ADNTGkt/src/main/java/frc/robot/RobotContainer.java
@@ -6,24 +6,42 @@ package frc.robot;
 
 import java.io.File;
 
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.wpilibj.Filesystem;
+import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+import frc.robot.Subsystems.Drive.DummyDriveSubsystem;
 import frc.robot.Subsystems.Drive.Swerve;
 import frc.robot.Subsystems.Drive.SwerveSprkMx;
 import frc.robot.Subsystems.Drive.Auto.Autos;
+import swervelib.SwerveDrive;
 
 
 /** Add your docs here. */
 public class RobotContainer {
+
+    public RobotContainer() {
+        configureBindings();
+    }
+
+    XboxController driverXbox = new XboxController(0);
+
     
-    private static final SwerveSprkMx driveBase = new 
+    public static final SwerveSprkMx driveBase = new 
     SwerveSprkMx(new File(Filesystem.getDeployDirectory(), "swerve"));
+
+    public static final DummyDriveSubsystem sub = new DummyDriveSubsystem();
 
     public static final Swerve Swerve = new
     Swerve(driveBase, driveBase);
 
-    public Command getAutonomousCommand(){
-        return Autos.exampleAuto(driveBase);
+    public Command getAutonomousCommand() {
+        return Autos.exampleAuto(driveBase, sub);
     }
 
+    private void configureBindings() {
+    }
 }
--- offseasonSwerveStuff/YAGSL+ADNTGkt/src/main/java/frc/robot/Subsystems/Drive/Auto/Autos.java
+++ offseasonSwerveStuff/YAGSL+ADNTGkt/src/main/java/frc/robot/Subsystems/Drive/Auto/Autos.java
@@ -4,6 +4,7 @@
 
 package frc.robot.Subsystems.Drive.Auto;
 
+import java.io.IOException;
 import java.util.List;
 
 import com.pathplanner.lib.PathConstraints;
@@ -14,22 +15,23 @@ import com.pathplanner.lib.auto.PIDConstants;
 import com.pathplanner.lib.auto.SwerveAutoBuilder;
 
 import edu.wpi.first.apriltag.AprilTagFieldLayout;
+import edu.wpi.first.apriltag.AprilTagFields;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj2.command.Commands;
+import frc.robot.Subsystems.Drive.DummyDriveSubsystem;
 import frc.robot.Subsystems.Drive.SwerveSprkMx;
 import frc.robot.Util.Constants.Auton;
 
 /** Add your docs here. */
 public final class Autos {
-    
     private static AprilTagFieldLayout aprilTagField = null;
 
     /**
      * @param swerve
      */
-    public static CommandBase exampleAuto(SwerveSprkMx swerve) { 
+    public static CommandBase exampleAuto(SwerveSprkMx swerve, DummyDriveSubsystem sub) { 
         boolean onTheFly = false;
         PathPlannerTrajectory example;
         if (onTheFly) {
@@ -44,14 +46,38 @@ public final class Autos {
             List<PathPlannerTrajectory> example1 = PathPlanner.loadPathGroup("SamplePath", new PathConstraints(4, 3));
             
             SwerveAutoBuilder autoBuilder = new SwerveAutoBuilder(swerve::getPose, swerve::resetOdometry, new PIDConstants(Auton.yAutoPID.p, Auton.yAutoPID.i, Auton.yAutoPID.d), new PIDConstants(Auton.angleAutoPID.p, Auton.angleAutoPID.i, Auton.angleAutoPID.d), swerve::setChassisSpeeds, null, true);  
-            
-            autoBuilder.fullAuto(example1);
 
             return Commands.sequence(autoBuilder.fullAuto(example1));
         }
 
-        return Commands.sequence(new FollowTrajectory(swerve, example, true));
+        return Commands.sequence(new FollowTrajectory(sub, swerve, example, true));
         
+    }
+
+    /**
+     * {@link FollowTrajectory}
+     * @param swerve
+     * @param id
+     * @param rotation
+     * @param holonomicRotation
+     * @param offset
+     * @return {@link FollowTrajectory}
+     */
+    public static CommandBase DriveToAprilTag(DummyDriveSubsystem sub, SwerveSprkMx swerve, int id, Rotation2d rotation, Rotation2d holonomicRotation, Translation2d offset) {
+        if (aprilTagField == null) {
+              try {
+                aprilTagField = AprilTagFields.k2023ChargedUp.loadAprilTagLayoutField();
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+            PathPlannerTrajectory path = PathPlanner.generatePath(new PathConstraints(4, 3), false, 
+                                                                PathPoint.fromCurrentHolonomicState(swerve.getPose(), swerve.getRobotVelocity()), 
+                                                                new PathPoint(aprilTagField.getTagPose(1).get().getTranslation().toTranslation2d().plus(new Translation2d(0,0)), new Rotation2d(0), new Rotation2d(0)));
+
+        return Commands.sequence(new FollowTrajectory(sub, swerve, path, true));            
+
     }
 
 }
