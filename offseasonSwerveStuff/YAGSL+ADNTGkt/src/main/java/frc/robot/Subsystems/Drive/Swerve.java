93b123d0b515c9282a60bdc81bedccb238c27a1b
masonmm3 <53064865+masonmm3@users.noreply.github.com>
2023-08-29 15:31:06 -0400

    limeLightImplementation

--- offseasonSwerveStuff/YAGSL+ADNTGkt/src/main/java/frc/robot/Subsystems/Drive/Swerve.java
+++ offseasonSwerveStuff/YAGSL+ADNTGkt/src/main/java/frc/robot/Subsystems/Drive/Swerve.java
@@ -5,14 +5,21 @@
 package frc.robot.Subsystems.Drive;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.littletonrobotics.junction.Logger;
 
 import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.geometry.Translation3d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.math.util.Units;
+import edu.wpi.first.networktables.DoubleArrayEntry;
+import edu.wpi.first.networktables.IntegerEntry;
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableInstance;
 import frc.robot.Util.Constants.Swrv_STG;
 import swervelib.SwerveController;
 import swervelib.math.Matter;
@@ -23,18 +30,41 @@ public class Swerve {
     private SwerveIO io;
     private SwerveSprkMx swerve;
     private SwerveIOInputsAutoLogged inputs = new SwerveIOInputsAutoLogged();
+    private static String tableName = "limelight";
+    private DoubleArrayEntry botPosEntry;
+    private final IntegerEntry idEntry;
+    private Pose2d botPose2d;
 
     public Swerve(SwerveIO io, SwerveSprkMx swerve){
         this.io = io;
         this.swerve = swerve;
+
+
+        NetworkTable limelight = NetworkTableInstance.getDefault().getTable(tableName);
+        botPosEntry = limelight.getDoubleArrayTopic("botpose").getEntry(new double[] {});
+        idEntry = limelight.getIntegerTopic("tid").getEntry(0);
+
     }
 
     public void PeriodicSwerve() {
+
+        double[] botposeData = botPosEntry.get();
+        if(botposeData.length !=6) {
+            botPose2d = new Pose2d(0, 0, Rotation2d.fromDegrees(0));;
+        } else {
+            botPose2d = new Pose2d(botposeData[0], botposeData[1], Rotation2d.fromDegrees(botposeData[5]));
+        }
+        
         swerve.periodicTask();
         Logger.getInstance().recordOutput("Odometry", swerve.getPose());
 
         io.updateInputs(inputs);
         Logger.getInstance().processInputs("Drive", inputs);
+
+        
+        Logger.getInstance().recordOutput("LimePose", botPose2d);
+
+
 
     }
 
