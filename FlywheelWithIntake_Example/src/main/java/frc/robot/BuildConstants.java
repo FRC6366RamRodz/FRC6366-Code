1b569efb6bc46ed59268f86d0b4e3bfc799c38e2
masonmm3 <53064865+masonmm3@users.noreply.github.com>
2024-01-19 13:53:17 -0500

    now its fixed

--- FlywheelWithIntake_Example/src/main/java/frc/robot/BuildConstants.java
+++ FlywheelWithIntake_Example/src/main/java/frc/robot/BuildConstants.java
@@ -1,17 +1,19 @@
 package frc.robot;
 
-/** Automatically generated file containing build version information. */
+/**
+ * Automatically generated file containing build version information.
+ */
 public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "FlywheelWithIntake_Example";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 13;
-  public static final String GIT_SHA = "3eaaa657b4abde09b0193f59c3aacb05892d35d4";
-  public static final String GIT_DATE = "2024-01-19 13:24:10 EST";
+  public static final int GIT_REVISION = 14;
+  public static final String GIT_SHA = "4576873b88d8b4490240e1540fcb0f2e2601caef";
+  public static final String GIT_DATE = "2024-01-19 13:50:56 EST";
   public static final String GIT_BRANCH = "2024";
-  public static final String BUILD_DATE = "2024-01-19 13:46:57 EST";
-  public static final long BUILD_UNIX_TIME = 1705690017056L;
+  public static final String BUILD_DATE = "2024-01-19 13:52:57 EST";
+  public static final long BUILD_UNIX_TIME = 1705690377106L;
   public static final int DIRTY = 1;
 
-  private BuildConstants() {}
+  private BuildConstants(){}
 }
