2305dc56c40172bbeef228fc13ad9f723f80a86a
masonmm3 <53064865+masonmm3@users.noreply.github.com>
2024-03-15 11:52:34 -0400

    updates to auto paths

--- FrankTheCrank/src/main/java/frc/robot/Robot.java
+++ FrankTheCrank/src/main/java/frc/robot/Robot.java
@@ -14,9 +14,7 @@
 package frc.robot;
 
 import edu.wpi.first.networktables.NetworkTableInstance;
-import edu.wpi.first.wpilibj.Compressor;
 import edu.wpi.first.wpilibj.GenericHID.RumbleType;
-import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj.PowerDistribution.ModuleType;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
