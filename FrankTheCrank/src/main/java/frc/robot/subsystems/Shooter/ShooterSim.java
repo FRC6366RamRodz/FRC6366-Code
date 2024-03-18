4d76c9946ec3cf2ac9790e6951bb45439350bd0e
masonmm3 <53064865+masonmm3@users.noreply.github.com>
2024-03-18 10:47:47 -0400

    autonomous stuff

--- FrankTheCrank/src/main/java/frc/robot/subsystems/Shooter/ShooterSim.java
+++ FrankTheCrank/src/main/java/frc/robot/subsystems/Shooter/ShooterSim.java
@@ -15,14 +15,14 @@ import edu.wpi.first.wpilibj.simulation.SingleJointedArmSim;
 /** Add your docs here. */
 public class ShooterSim implements ShooterIO {
   private SingleJointedArmSim shooterAngle =
-      new SingleJointedArmSim(DCMotor.getFalcon500(1), 125, 0.07480059831487, Units.inchesToMeters(21.729), Units.degreesToRadians(-50), Units.degreesToRadians(90), true, Units.degreesToRadians(-50));
+      new SingleJointedArmSim(DCMotor.getFalcon500(1), 200, 0.7480059831487, Units.inchesToMeters(21.729), Units.degreesToRadians(-50), Units.degreesToRadians(90), true, Units.degreesToRadians(-50));
   private FlywheelSim intake = new FlywheelSim(DCMotor.getNEO(1), 5, 0.004);
   private FlywheelSim handler = new FlywheelSim(DCMotor.getNEO(1), 5, 0.04);
   private FlywheelSim feeder = new FlywheelSim(DCMotor.getNeo550(1), 15, 0.04);
   private FlywheelSim topShooter = new FlywheelSim(DCMotor.getKrakenX60(1), 2, 0.004);
   private FlywheelSim bottomShooter = new FlywheelSim(DCMotor.getKrakenX60(1), 2, 0.004);
 
-  private PIDController anglePID = new PIDController(0.1, 0, 0.0000);
+  private PIDController anglePID = new PIDController(0.2, 0, 0.0000);
   private ArmFeedforward angleFeedForward = new ArmFeedforward(0, 0.0405, 8);
 
   private PIDController topPID = new PIDController(0.01, 0, 0.0000);
