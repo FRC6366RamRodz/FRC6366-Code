d78f10ff1c29746ca217bd0b342ed6295f03850d
FRC6366RamRodz <132411530+FRC6366RamRodz@users.noreply.github.com>
2024-03-29 22:17:08 -0400

    rio 2 finally made it work

--- DiffDrive_Example(tank)/src/main/java/frc/robot/BuildConstants.java
+++ DiffDrive_Example(tank)/src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "DiffDrive_Example(tank)";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 91;
-  public static final String GIT_SHA = "07a006718105f5f5b08c28865687bb00f5c150fe";
-  public static final String GIT_DATE = "2024-03-19 17:24:24 EDT";
+  public static final int GIT_REVISION = 102;
+  public static final String GIT_SHA = "c2ab71795bf623ee8fda9bec28ebb8b8882ebb26";
+  public static final String GIT_DATE = "2024-03-27 20:24:01 EDT";
   public static final String GIT_BRANCH = "2024";
-  public static final String BUILD_DATE = "2024-03-19 19:29:08 EDT";
-  public static final long BUILD_UNIX_TIME = 1710890948332L;
+  public static final String BUILD_DATE = "2024-03-28 19:09:46 EDT";
+  public static final long BUILD_UNIX_TIME = 1711667386636L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
